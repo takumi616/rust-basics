959b469deb9a88fbc34d0e68d4af624d6aadaabd
takumi616 <ohohyeah321@icloud.com>
2024-06-10 08:42:06 +0900

    Test hot to edit slice element.

--- src/slice_sample.rs
+++ src/slice_sample.rs
@@ -10,7 +10,23 @@ fn slice_from_vec() {
     println!("languages_slice: {:?}", languages_slice); //output => ["Rust", "C++", "Golang"]
 }
 
+fn edit_slice() {
+    let mut nums: Vec<i32> = vec![60, 66, 160, 166, 260];
+    println!("nums: {:?}", nums);
+
+    let nums_slice: &mut [i32] = &mut nums[0..4]; 
+    nums_slice[0] = 16;
+    println!("nums: {:?}", nums);
+
+    let _removed: i32 = nums.remove(2);
+    println!("nums: {:?}", nums);
+
+    nums.insert(4, 266);
+    println!("nums: {:?}", nums);
+}
+
 pub fn slice() {
     slice_from_array();
     slice_from_vec();
+    edit_slice();
 }
