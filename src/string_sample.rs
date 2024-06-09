32bf5b1816ef0f2253efe9bd149d78043f308c6a
takumi616 <ohohyeah321@icloud.com>
2024-06-09 10:31:45 +0900

    Use String type with Vec and for loops.

--- src/string_sample.rs
+++ src/string_sample.rs
@@ -1,7 +1,44 @@
-pub fn string() {
+fn simple_string() {
     let mut greeting: String = String::from("Hello");
     println!("greeting: {}", greeting);
 
     greeting.push_str(", world");
     println!("greeting: {}", greeting);
+}
+
+fn string_with_vec() {
+    let countries: Vec<String> = vec![
+        String::from("United states"),
+        String::from("Germany"),
+        String::from("Russia"),
+    ];
+    println!("countries: {:?}", countries);
+
+    //Use iter()
+    for country in countries.iter() {
+        match country.as_str() {
+            "United states" => println!("It's United states"),
+            "Germany" => println!("It's Germany"),
+            "Russia" => println!("It's Russia"),
+            _ => println!(""),
+        }
+    }
+    println!("countries: {:?}", countries);
+
+    //use into_iter()
+    for country in countries.into_iter() {
+        match country.as_str() {
+            "United states" => println!("It's United states"),
+            "Germany" => println!("It's Germany"),
+            "Russia" => println!("It's Russia"),
+            _ => println!(""),
+        }
+    }
+    //println!("countries: {:?}", countries);
+    println!("can not print countries because into_iter() consumes each value of Vec<String>.")
+}
+
+pub fn string() {
+    simple_string();
+    string_with_vec();
 }
