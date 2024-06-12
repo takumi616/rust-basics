f5eab4b23cb7ce1b920659406b6356017235c458
takumi616 <ohohyeah321@icloud.com>
2024-06-12 23:47:22 +0900

    Access vector type element using get method.

--- src/vec_sample.rs
+++ src/vec_sample.rs
@@ -26,6 +26,22 @@ fn access_elements() {
     let languages: Vec<&str> = vec!["C++", "Rust", "Python"];
     let language: &str = languages[1];
     println!("accessed element: {}", language);
+
+    //If given a position, returns a reference to the element 
+    //at that position or None if out of bounds
+    let another_language:Option<&&str> = languages.get(0);
+    match another_language {
+        Some(accessed) => println!("accessed element using get method: {}", accessed),
+        None => println!("not found"),
+    }
+
+    //If given a range, returns the subslice corresponding to that range, 
+    //or None if out of bounds.
+    let languages_slice:Option<&[&str]> = languages.get(1..3);
+    match languages_slice {
+        Some(accessed_languages) => println!("accessed elements creating slice: {:?}", accessed_languages),
+        None => println!("not found"),
+    }
 }
 
 pub fn vec() {
