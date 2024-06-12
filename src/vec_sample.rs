ce6ecec21d7648a6f3e6d1d025bf12b54478c9a8
takumi616 <ohohyeah321@icloud.com>
2024-06-12 22:41:34 +0900

    Access the element of vector type.

--- src/vec_sample.rs
+++ src/vec_sample.rs
@@ -1,27 +1,33 @@
-fn vec_with_string() {
-    let mut genais: Vec<String> = Vec::new();
-    genais.push(String::from("Llama"));
-    genais.push(String::from("Gemini"));
-    println!("genais: {:?}", genais);
+// fn vec_with_string() {
+//     let mut genais: Vec<String> = Vec::new();
+//     genais.push(String::from("Llama"));
+//     genais.push(String::from("Gemini"));
+//     println!("genais: {:?}", genais);
     
-    let mut additional: Vec<String> = Vec::new();
-    additional.push(String::from("Chatgpt"));
-    additional.push(String::from("Cohere"));
-    println!("additional: {:?}", additional);
+//     let mut additional: Vec<String> = Vec::new();
+//     additional.push(String::from("Chatgpt"));
+//     additional.push(String::from("Cohere"));
+//     println!("additional: {:?}", additional);
 
-    genais.append(&mut additional);
-    println!("genais: {:?}", genais);
-}
+//     genais.append(&mut additional);
+//     println!("genais: {:?}", genais);
+// }
 
-fn vec_with_str() {
-    let mut languages: Vec<&str> = vec!["Rust", "Golang", "Python"];
-    println!("languages: {:?}", languages);
+// fn vec_with_str() {
+//     let mut languages: Vec<&str> = vec!["Rust", "Golang", "Python"];
+//     println!("languages: {:?}", languages);
 
-    languages.push("C++");
-    println!("languages: {:?}", languages);
+//     languages.push("C++");
+//     println!("languages: {:?}", languages);
+// }
+
+//Access the elemen of Vector
+fn access_elements() {
+    let languages: Vec<&str> = vec!["C++", "Rust", "Python"];
+    let language: &str = languages[1];
+    println!("accessed element: {}", language);
 }
 
 pub fn vec() {
-    vec_with_string();
-    vec_with_str();
+    access_elements();
 }
